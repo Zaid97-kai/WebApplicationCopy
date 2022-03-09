e5f21c4ac3840ff5a2a9de43a00ebe27322376f6
Заид Мингалиев <zzmingaliev@outlook.com>
2022-03-09 12:32:20 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string author { get; set; }
+        public int price { get; set; }
+
+    }
+}
